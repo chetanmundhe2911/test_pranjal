567e37fedbacaa4df0e66775b779f24761e04a26
chetan mundhe <chetanmundhe2911@gmail.com>
2022-10-11 19:17:54 +0530

    removed

--- main.py
+++ main.py
@@ -3,13 +3,3 @@ import pandas as pd
 import numpy as np
 
 import seaborn as sns
-
-print('hello world')
-
-print('pranjal kirange')
-
-print('chetan')
-
-print('last time..dfdfdf')
-
-print("main_python")
